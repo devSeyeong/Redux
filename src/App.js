dc3050711b7a26b94eb4e3babcdca3674c0a503a
mseyeong <mseyeong3131@mek-ics.com>
2023-04-21 18:54:51 +0900

    todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import CounterContainer from "./containers/CounterContainer";
+import React from 'react';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from "./containers/TodoContainer";
+
 
 function App() {
-  return (
-    <div>
-      <CounterContainer />
-    </div>
-  );
+    return (
+        <div>
+            <CounterContainer />
+            <hr />
+            <TodosContainer />
+        </div>
+    );
 }
 
 export default App;
